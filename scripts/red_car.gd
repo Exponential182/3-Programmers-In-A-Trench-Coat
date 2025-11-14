f5ee80c2b8ab1856c6c3a497f6c1d52ffb77ef8e
Flamerider347 <flamerider995@gmail.com>
2025-11-14 21:16:06 +1300

    car stuff

--- scripts/red_car.gd
+++ scripts/red_car.gd
@@ -8,7 +8,11 @@ var steering_multiplier = 0.4
 func _physics_process(delta: float) -> void:
 	steering = lerp(steering, Input.get_axis("right","left") * steering_multiplier, 5 * delta)
 	var acceleration = Input.get_axis("backward","forward")
+
 	var rpm = $BL_Wheel.get_rpm()
 	$BL_Wheel.engine_force = acceleration * max_torque * (1-rpm/max_rpm)
 	rpm = $BR_Wheel.get_rpm()
 	$BR_Wheel.engine_force = acceleration * max_torque * (1-rpm/max_rpm)
+	if not Input.is_action_pressed("forward") and not Input.is_action_pressed("backward"):
+		$BR_Wheel.engine_force = lerp($BR_Wheel.engine_force,0.0,5.0)
+		$BL_Wheel.engine_force = lerp($BL_Wheel.engine_force,0.0,5.0)
